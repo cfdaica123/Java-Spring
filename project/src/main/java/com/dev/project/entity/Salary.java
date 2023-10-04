c9920eaf26acf600fad5de72568426def46fee02
cfdaica123 <soncaodarkin3@gmail.com>
2023-10-04 19:21:11 +0700

    salary

--- project/src/main/java/com/dev/project/entity/Salary.java
+++ project/src/main/java/com/dev/project/entity/Salary.java
@@ -26,43 +26,46 @@ public class Salary {
     @Column(name = "end_date")
     private LocalDate endDate;
 
+    @Column(name = "advance_amount")
+    private BigDecimal advanceAmount;
+
+    @Column(name = "advance_date")
+    private LocalDate advanceDate;
+
+    @Column(name = "net_salary")
+    private BigDecimal netSalary;
+
+    @Column(name = "total_salary")
+    private BigDecimal totalSalary;
+
     // Getters and setters
     public Long getSalaryId() {
         return salaryId;
     }
-    
     public void setSalaryId(Long salaryId) {
         this.salaryId = salaryId;
     }
-    
     public Employee getEmployee() {
         return employee;
     }
-    
     public void setEmployee(Employee employee) {
         this.employee = employee;
     }
-    
     public BigDecimal getAmount() {
         return amount;
     }
-    
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
-    
     public LocalDate getStartDate() {
         return startDate;
     }
-    
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
-    
     public LocalDate getEndDate() {
         return endDate;
     }
-    
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
@@ -75,6 +78,10 @@ public class Salary {
                 ", amount=" + amount +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
+                ", advanceAmount=" + advanceAmount +
+                ", advanceDate=" + advanceDate +
+                ", netSalary=" + netSalary +
+                ", totalSalary=" + totalSalary +
                 '}';
     }
 }
